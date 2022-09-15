5d224c94d4eb05a2266f4b8deb4661e18f973481
Nathan Kagi <nathan.kagi@rigtechnologies.com.au>
2022-09-15 08:55:35 +0800

    initial commit with compiled .proto file

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import addressbook_pb2
+print("")
+person = addressbook_pb2.Person()
+person.id = 1234
+person.name = "John Doe"
+person.email = "jdoe@example.com"
+phone = person.phones.add()
+phone.number = "555-4321"
+phone.type = addressbook_pb2.Person.HOME
+print(person)
+
+
+if __name__ == "__main__.py":
+    pass
